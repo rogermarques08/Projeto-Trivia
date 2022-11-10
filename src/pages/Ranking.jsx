22ceab9e7d50107a4c2aa94dba1d12787e502ecf
Erick Argollo <argolloerickdev@gmail.com>
2022-11-10 16:09:31 -0300

    feat[project]: Requisito 18

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,11 +1,33 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 
 class Ranking extends React.Component {
+  playAgain = () => {
+    const { history } = this.props;
+    localStorage.removeItem('token');
+    history.push('/');
+  };
+
   render() {
     return (
-      <p>Ranking</p>
+      <div>
+        <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ this.playAgain }
+        >
+          Play Again
+
+        </button>
+      </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape().isRequired,
+};
+
 export default connect()(Ranking);
