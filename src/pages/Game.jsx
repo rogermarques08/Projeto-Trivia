c0522ce63e8e20fda3524585efebdc67fb276c0a
Erick Argollo <argolloerickdev@gmail.com>
2022-11-10 10:07:43 -0300

    feat[project]: Requisito 6 Erick, Matheus, Roger, Miguel

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,15 +1,51 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
+import fetchTriviaQuestions from '../services/fetchTriviaQuestions';
+import Question from '../components/Question';
 
 class Game extends React.Component {
+  state = {
+    questions: [],
+  };
+
+  componentDidMount() {
+    this.getApiQuestions();
+  }
+
+  getApiQuestions = async () => {
+    const { history } = this.props;
+    const RESPONSE_CODE_ERROR = 3;
+    const token = localStorage.getItem('token');
+    const response = await fetchTriviaQuestions(token);
+    if (response.response_code === RESPONSE_CODE_ERROR) {
+      localStorage.removeItem('token');
+      history.push('/');
+    } else {
+      this.setState({
+        questions: response.results,
+      });
+    }
+  };
+
   render() {
+    const { questions } = this.state;
     return (
       <div>
         <h1>game</h1>
         <Header />
+
+        {
+          questions.length > 0 && <Question questions={ questions } />
+        }
+
       </div>
     );
   }
 }
+
+Game.propTypes = {
+  history: PropTypes.shape().isRequired,
+};
 export default connect()(Game);
---
+++ src/services/fetchTriviaQuestions.js
@@ -0,0 +1,8 @@
+const fetchTriviaQuestions = async (token) => {
+  const url = `https://opentdb.com/api.php?amount=5&token=${token}`;
+  const request = await fetch(url);
+  const response = await request.json();
+  return response;
+};
+
+export default fetchTriviaQuestions;
