975f57469b50fcd02bec88b2bbb7ce4075cd18af
Roger Marques <marquesroger226@gmail.com>
2022-11-10 15:39:07 -0300

    feat[project]: Adiciona requisito 14
    Roger,Pedro

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,16 +1,32 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
   render() {
+    const { player } = this.props;
+    console.log(player);
     return (
       <div>
         <Header />
         <h1 data-testid="feedback-text">Feedback</h1>
-
+        <h2 data-testid="feedback-total-score">{player.score}</h2>
+        <h2 data-testid="feedback-total-question">{player.assertions}</h2>
       </div>
     );
   }
 }
-export default connect()(Feedback);
+
+Feedback.propTypes = {
+  player: PropTypes.shape({
+    assertions: PropTypes.number.isRequired,
+    score: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  player: state.player,
+});
+
+export default connect(mapStateToProps)(Feedback);
